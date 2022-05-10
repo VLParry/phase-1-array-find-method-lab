a3ae19608dd567d02b77e9c2ec6c4686f0d4b972
Victoria Parry <vlparry@gmail.com>
2022-05-09 21:53:30 -0600

    commit tree 5faf31b50ee3f8a0080fe5b0d35f3c5c71802af9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+
+
+// function superbowlWin(record) {
+//     let win = record.find(result => record.result === "W")
+//     if (win === true) {
+//         record.year
+//     } else {
+//         undefined
+//     }
+// }
+
+
+// function superbowlWin(record) {
+//     for (const team of record) {
+//         if (record.find(result => record.result === "W")) {
+//             return record.year
+//         } else {
+//             return undefined
+//         }
+//     }
+// }
+// superbowlWin(record)
+
+// function superbowlWin(record)
+// return record.result === "W"
+
+// console.log(record.find("W").year)
+
+
+
+function superbowlWin(array) {
+    const win = record.find(superbowlWin).year;
+    return array.result === "W";
+}
